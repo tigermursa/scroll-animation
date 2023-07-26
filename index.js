6acc99706504ca1729b0e15a0bd839499782bfea
mursalin@gmail.com <MURSA>
2023-07-26 19:44:53 +0600

    created the scroll effect for larger device

---
+++ index.js
@@ -0,0 +1,55 @@
+$(function () {
+  // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: "onLeave",
+      duration: "100%", // this works just fine with duration 0 as well
+      // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+      // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+    },
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i],
+    })
+      .setPin(slides[i], { pushFollowers: false })
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+  }
+});
+
+// Add this to your existing index.js
+
+const sections = document.querySelectorAll(".panel");
+const dots = document.querySelectorAll(".dot");
+
+function setActiveDot(index) {
+    dots.forEach((dot, i) => {
+        dot.classList.toggle("active", i === index);
+    });
+}
+
+function updateActiveSection() {
+    const sectionHeight = window.innerHeight;
+    const currentScroll = window.scrollY;
+
+    let activeSectionIndex = 0;
+    sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= sectionHeight) {
+            activeSectionIndex = index;
+        }
+    });
+
+    setActiveDot(activeSectionIndex);
+}
+
+document.addEventListener("scroll", updateActiveSection);
+
+
