35cf780980b858a2a37e1016bb1021af3b229b72
mursalin@gmail.com <MURSA>
2023-07-27 09:14:28 +0600

    removed bootstrap codes

--- index.js
+++ index.js
@@ -30,26 +30,34 @@ const sections = document.querySelectorAll(".panel");
 const dots = document.querySelectorAll(".dot");
 
 function setActiveDot(index) {
-    dots.forEach((dot, i) => {
-        dot.classList.toggle("active", i === index);
-    });
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
 }
 
 function updateActiveSection() {
-    const sectionHeight = window.innerHeight;
-    const currentScroll = window.scrollY;
+  const sectionHeight = window.innerHeight;
+  const currentScroll = window.scrollY;
 
-    let activeSectionIndex = 0;
-    sections.forEach((section, index) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= sectionHeight) {
-            activeSectionIndex = index;
-        }
-    });
+  let activeSectionIndex = 0;
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= sectionHeight) {
+      activeSectionIndex = index;
+    }
+  });
 
-    setActiveDot(activeSectionIndex);
+  setActiveDot(activeSectionIndex);
 }
 
 document.addEventListener("scroll", updateActiveSection);
 
-
+// aos code
+AOS.init({
+  offset: 150, // Change the distance (in pixels) from the element's bottom to trigger the animation
+  duration: 800, // Duration of the animation (in milliseconds)
+  easing: "ease-in-out", // Easing function to control the animation's acceleration and deceleration
+  once: false, // Set to "false" to trigger the animations on every scroll (both up and down)
+  mirror: true, // Set to "true" to mirror the animations on scroll up and scroll down
+  // Add any other custom options here as needed
+});
